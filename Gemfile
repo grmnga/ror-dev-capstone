dad1cf935222d5035d983b6e7969366fac185f46
Anna <anna@grmn.ga>
2020-06-03 16:12:44 +0500

    added rack-cors gem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.11.3'
 
 gem 'rails-api', '~>0.4', '>=0.4.0'
+gem 'rack-cors', '~>0.4', '>=0.4.0', :require => 'rack/cors'
 
 group :development do
   gem 'spring', '~>2.0', '>=2.0.0'
--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,16 @@ module CapstoneDemoapp3
     Mongoid.load!('./config/mongoid.yml')
     config.generators {|g| g.orm :active_record}
 
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+
+        resource '/api/*',
+                 :headers => :any,
+                 :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
