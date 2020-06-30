a7775e5a6629ccab80d7d19a5066c3fb0da7284b
Anna <anna@grmn.ga>
2020-06-30 12:43:26 +0500

    cities CRUD

--- app/assets/javascripts/spa-demo/cities/cities.service.js
+++ app/assets/javascripts/spa-demo/cities/cities.service.js
@@ -10,18 +10,17 @@
         return $resource(APP_CONFIG.server_url + "/api/cities/:id",
             { id: '@id'},
             {
-                update: { method: "PUT" }
-                // ,
-                //     transformRequest: buildNestedBody },
-                // save: { method: "POST",
-                //     transformRequest: buildNestedBody }
+                update: { method: "PUT",
+                    transformRequest: buildNestedBody },
+                save: { method: "POST",
+                    transformRequest: buildNestedBody }
             }
         );
     }
 
     //nests the default payload below a "city" element
     //as required by default by Rails API resources
-    // function buildNestedBody(data) {
-    //     return angular.toJson({city: data})
-    // }
+    function buildNestedBody(data) {
+        return angular.toJson({city: data})
+    }
 })();
